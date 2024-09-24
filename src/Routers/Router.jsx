2f0fd5d79b1c93b32838a682d0bf5a5ab4805ebd
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-25 04:04:31 +0600

    Set route and create layout

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -3,12 +3,13 @@ import Layout from "../layout/layout";
 import Home from "../pages/Home/Home";
 import LogIn from "../pages/LogIn/LogIn";
 import Register from "../pages/Register/Register";
+import Error404 from "../pages/Error404/Error404";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement:
+    errorElement:<Error404/>,
     children: [
       {
         path: "/",
