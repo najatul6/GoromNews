24e3ecbb98bd43c9fb20e076318b97304eccaaae
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-25 22:45:05 +0600

    Create breaking news lines

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -2,9 +2,11 @@ import Marquee from "react-fast-marquee"
 
 const BreakingNews = () => {
   return (
-    <div>
+    <div className="flex gap-2 justify-center items-center p-4 bg-[#F3F3F3]">
         <p className="py-2 px-3 bg-light-blue text-white text-xl font-bold">Latest</p>
-        <Marquee></Marquee>
+        <Marquee className="font-bold uppercase">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit accusamus facilis corrupti aliquam, quae unde laborum rerum aliquid quia cumque esse voluptate voluptatum sapiente quaerat quod consectetur a, voluptates quibusdam obcaecati. Facere eveniet atque voluptate officia, reiciendis perspiciatis omnis repellat itaque distinctio corporis consequuntur, placeat natus ad? Ducimus, culpa provident?
+        </Marquee>
     </div>
   )
 }
--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -2,7 +2,7 @@ import moment from "moment"
 
 const Header = () => {
   return (
-    <div className="flex flex-col justify-center items-center pb-5">
+    <div className="flex flex-col justify-center items-center pb-2">
         <img src="/GoromNews.png" alt="gorom News" className="h-32" />
      <p>Reporting Truth Without Bias or Boundaries.</p>
      <p className="text-xl font-bold">{moment().format("dddd, MMMM D,YYYY")}</p>
