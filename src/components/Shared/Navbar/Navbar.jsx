1ca03f88934951113262bd592878a175d27e1259
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-25 23:13:14 +0600

    set effect for menu

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,46 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
+  const menu = (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "py-3 px-3 bg-zinc-200 font-bold rounded-xl text-center"
+            : " py-3 px-3 hover:bg-zinc-200 font-semibold rounded-xl text-center"
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "py-3 px-3 bg-zinc-200 font-bold rounded-xl text-center"
+            : " py-3 px-3 hover:bg-zinc-200 font-semibold rounded-xl text-center"
+        }
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/career"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "py-3 px-3 bg-zinc-200 font-bold rounded-xl text-center"
+            : "py-3 px-3 hover:bg-zinc-200 font-semibold rounded-xl text-center"
+        }
+      >
+        Career
+      </NavLink>
+    </>
+  );
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -23,52 +65,15 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menu}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1 space-x-3">{menu}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn btn-sm">Button</a>
       </div>
     </div>
   );
