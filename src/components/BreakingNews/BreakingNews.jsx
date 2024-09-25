51fb2944c1462dcc943095a185c4fde7b215ef59
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-25 22:38:51 +0600

    Add custom color

---
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -0,0 +1,12 @@
+import Marquee from "react-fast-marquee"
+
+const BreakingNews = () => {
+  return (
+    <div>
+        <p className="py-2 px-3">Latest</p>
+        <Marquee></Marquee>
+    </div>
+  )
+}
+
+export default BreakingNews
