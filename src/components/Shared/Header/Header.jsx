b37ef6ccc5f6f031318262b7e26051b787fb4dc7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-25 22:54:16 +0600

    update theme color and header sections classes

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -2,9 +2,9 @@ import moment from "moment"
 
 const Header = () => {
   return (
-    <div className="flex flex-col justify-center items-center pb-2">
+    <div className="flex flex-col justify-center items-center pb-4">
         <img src="/GoromNews.png" alt="gorom News" className="h-32" />
-     <p>Reporting Truth Without Bias or Boundaries.</p>
+     <p className="my-2">Reporting Truth Without Bias or Boundaries.</p>
      <p className="text-xl font-bold">{moment().format("dddd, MMMM D,YYYY")}</p>
     </div>
   )
