276cf9085f124e864acb33886826a773977584e5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-25 23:38:09 +0600

    add speed and pause on hover function in marquee section

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -1,14 +1,24 @@
-import Marquee from "react-fast-marquee"
+import Marquee from "react-fast-marquee";
 
 const BreakingNews = () => {
   return (
     <div className="flex gap-2 justify-center items-center p-4 bg-[#F3F3F3]">
-        <p className="py-2 px-3 bg-light-blue text-white text-xl font-bold">Latest</p>
-        <Marquee className="font-bold uppercase">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit accusamus facilis corrupti aliquam, quae unde laborum rerum aliquid quia cumque esse voluptate voluptatum sapiente quaerat quod consectetur a, voluptates quibusdam obcaecati. Facere eveniet atque voluptate officia, reiciendis perspiciatis omnis repellat itaque distinctio corporis consequuntur, placeat natus ad? Ducimus, culpa provident?
+      <p className="py-2 px-3 bg-light-blue text-white text-xl font-bold">
+        Latest
+      </p>
+      <div className="font-bold uppercase">
+        <Marquee speed={120} pauseOnHover={true}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit
+          accusamus facilis corrupti aliquam, quae unde laborum rerum aliquid
+          quia cumque esse voluptate voluptatum sapiente quaerat quod
+          consectetur a, voluptates quibusdam obcaecati. Facere eveniet atque
+          voluptate officia, reiciendis perspiciatis omnis repellat itaque
+          distinctio corporis consequuntur, placeat natus ad? Ducimus, culpa
+          provident?
         </Marquee>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BreakingNews
+export default BreakingNews;
