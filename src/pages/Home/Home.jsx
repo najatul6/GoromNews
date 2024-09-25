f79ba9846d8be74c509db62eef8051d4f80b5fee
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-25 22:10:20 +0600

    designed home layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,24 @@
+import Header from "../../components/Shared/Header/Header";
+import LeftSide from "../../components/Shared/LeftSide/LeftSide";
+import Navbar from "../../components/Shared/Navbar/Navbar";
+import RightSide from "../../components/Shared/RightSide/RightSide";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Header />
+      <Navbar />
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+        <div>
+          <RightSide />
+        </div>
+        <div className="md:col-span-2">News Comming Soon</div>
+        <div>
+          <LeftSide />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
