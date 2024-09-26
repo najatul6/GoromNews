8fc0afc8c74be73ac2a7103604c195015a50e1d4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-26 23:54:09 +0600

    add google icon

--- src/components/Shared/LeftSide/LeftSide.jsx
+++ src/components/Shared/LeftSide/LeftSide.jsx
@@ -1,10 +1,12 @@
+import { FcGoogle } from "react-icons/fc";
+
 const LeftSide = () => {
   return (
     <div>
       <div>
         <h2>Login With</h2>
         <div>
-          <button className="btn ">Login with Google</button>
+          <button className="btn "><FcGoogle /> Login with Google</button>
           <button className="btn ">Login with Facebook</button>
         </div>
       </div>
