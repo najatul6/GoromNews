50899fd7204e3c0152a50b47b65785f875c2f7d4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-26 23:16:10 +0600

    update breaking news component

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -1,20 +1,41 @@
+import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 
 const BreakingNews = () => {
+  const [headline, setHeadline] = useState();
+  console.log(headline);
+  useEffect(() => {
+    fetch(
+      `${
+        import.meta.env.VITE_NEWS_URL
+      }category=general&lang=en&country=us&max=10&apikey=${
+        import.meta.env.VITE_NEWS_KEY
+      }`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const articleTitles = data.articles.map((article) => article.title); // Extract titles
+        setHeadline(articleTitles); // Set the titles into state
+      })
+      .catch((error) => {
+        console.error("Error fetching news:", error);
+      });
+  }, []);
   return (
-    <div className="flex gap-2 justify-center items-center p-4 bg-[#F3F3F3]">
+    <div className="flex justify-center items-center p-4 bg-[#F3F3F3] max-w-7xl border border-red-600">
       <p className="py-2 px-3 bg-light-blue text-white text-xl font-bold">
         Latest
       </p>
-      <div className="font-bold uppercase">
+      <div className="font-bold uppercase flex-1">
         <Marquee speed={120} pauseOnHover={true}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit
-          accusamus facilis corrupti aliquam, quae unde laborum rerum aliquid
-          quia cumque esse voluptate voluptatum sapiente quaerat quod
-          consectetur a, voluptates quibusdam obcaecati. Facere eveniet atque
-          voluptate officia, reiciendis perspiciatis omnis repellat itaque
-          distinctio corporis consequuntur, placeat natus ad? Ducimus, culpa
-          provident?
+       {/* <p>{headline}</p> */}
+          <p className="flex justify-center items-center border-red-600">
+            {headline?.length > 0
+              ? headline.map((title, index) => <p className="flex gap-2" key={index}>| {title} | </p>)
+              : "Loading Breaking news..."}
+          </p>
         </Marquee>
       </div>
     </div>
