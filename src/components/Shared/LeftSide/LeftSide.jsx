7eeaa0cd2b4769c9e34e25b33db42ae1bc5cde41
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-27 00:00:01 +0600

    set font styles

--- src/components/Shared/LeftSide/LeftSide.jsx
+++ src/components/Shared/LeftSide/LeftSide.jsx
@@ -7,8 +7,12 @@ const LeftSide = () => {
       <div className="w-full space-y-5">
         <h2 className="text-xl font-bold">Login With</h2>
         <div className="w-full space-y-5">
-          <button className="btn w-full btn-outline"><FcGoogle /> Login with Google</button>
-          <button className="btn w-full btn-outline"><FaFacebook /> Login with Facebook</button>
+          <button className="btn w-full btn-outline text-lg font-medium">
+            <FcGoogle /> Login with Google
+          </button>
+          <button className="btn w-full btn-outline text-lg font-medium">
+            <FaFacebook /> Login with Facebook
+          </button>
         </div>
       </div>
     </div>
