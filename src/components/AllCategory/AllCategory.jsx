ccac893abd59b121e49bd508b4e020a2ee419059
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-26 23:34:32 +0600

    get all categories data via fetch data

--- src/components/AllCategory/AllCategory.jsx
+++ src/components/AllCategory/AllCategory.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react"
+import { useState } from "react"
 
 const AllCategory = () => {
+    const [categories,setCategories]=useState([])
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+        .catch(err=>console.log(err))
+    },[])
   return (
-    <div>AllCategory</div>
+    <div>
+
+    </div>
   )
 }
 
