d6e81da5f2f3a9a93cfccdbfde9840687f004f99
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-26 23:51:52 +0600

    add log in button

--- src/components/Shared/LeftSide/LeftSide.jsx
+++ src/components/Shared/LeftSide/LeftSide.jsx
@@ -1,8 +1,15 @@
-
 const LeftSide = () => {
   return (
-    <div>LeftSide</div>
-  )
-}
+    <div>
+      <div>
+        <h2>Login With</h2>
+        <div>
+          <button className="btn btn-primary">Login with Google</button>
+          <button className="btn btn-primary">Login with Facebook</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LeftSide
+export default LeftSide;
