31e96b99c61ff84e17f9f76aae17672cd5fa309f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-26 23:49:46 +0600

    update right side menu

--- src/components/AllCategory/AllCategory.jsx
+++ src/components/AllCategory/AllCategory.jsx
@@ -1,21 +1,26 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import { useEffect } from "react";
+import { useState } from "react";
 
 const AllCategory = () => {
-    const [categories,setCategories]=useState([])
-    useEffect(()=>{
-        fetch('categories.json')
-        .then(res=>res.json())
-        .then(data=>setCategories(data))
-        .catch(err=>console.log(err))
-    },[])
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="flex flex-col justify-center items-start gap-2">
-        {categories.map((category)=>(
-            <p className="uppercase font-bold bg-[#E7E7E7] px-5 py-4 w-full rounded-xl text-light-gray hover:text-dark-black" key={category.id}>{category?.category}</p>
-        ))}
+      {categories.map((category) => (
+        <p
+          className="uppercase font-bold bg-[#E7E7E7] px-5 py-4 w-full rounded-xl text-light-gray hover:text-dark-black"
+          key={category.id}
+        >
+          {category?.category}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default AllCategory
+export default AllCategory;
--- src/components/Shared/RightSide/RightSide.jsx
+++ src/components/Shared/RightSide/RightSide.jsx
@@ -3,7 +3,7 @@ import AllCategory from "../../AllCategory/AllCategory"
 const RightSide = () => {
   return (
     <div>
-      <div>
+      <div className="space-y-5">
         <h2 className="text-xl font-bold">All Caterogy</h2>
         <AllCategory/>
       </div>
