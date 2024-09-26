2efb2c8882510396f6a285c53b114dc1c32c9221
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-26 23:23:13 +0600

    Update url for top headlines

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -2,40 +2,41 @@ import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 
 const BreakingNews = () => {
-  const [headline, setHeadline] = useState();
-  console.log(headline);
+  const [headlines, setHeadlines] = useState([]);
+  console.log(headlines);
+
   useEffect(() => {
     fetch(
       `${
-        import.meta.env.VITE_NEWS_URL
-      }category=general&lang=en&country=us&max=10&apikey=${
+        import.meta.env.VITE_TOP_NEWS_URL
+      }category=technology&lang=en&country=us&max=10&apikey=${
         import.meta.env.VITE_NEWS_KEY
       }`
     )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
+      .then((response) => response.json())
+      .then((data) => {
         const articleTitles = data.articles.map((article) => article.title); // Extract titles
-        setHeadline(articleTitles); // Set the titles into state
+        setHeadlines(articleTitles); // Set the titles into state
       })
       .catch((error) => {
         console.error("Error fetching news:", error);
       });
   }, []);
+
   return (
-    <div className="flex justify-center items-center p-4 bg-[#F3F3F3] max-w-7xl border border-red-600">
+    <div className="flex justify-center items-center p-4 bg-[#F3F3F3] max-w-7xl">
       <p className="py-2 px-3 bg-light-blue text-white text-xl font-bold">
         Latest
       </p>
       <div className="font-bold uppercase flex-1">
         <Marquee speed={120} pauseOnHover={true}>
-       {/* <p>{headline}</p> */}
-          <p className="flex justify-center items-center border-red-600">
-            {headline?.length > 0
-              ? headline.map((title, index) => <p className="flex gap-2" key={index}>| {title} | </p>)
-              : "Loading Breaking news..."}
-          </p>
+          {headlines.length > 0
+            ? headlines.map((title, index) => (
+                <span key={index} className="mx-2">
+                  | {title} |
+                </span>
+              ))
+            : "Loading Breaking news..."}
         </Marquee>
       </div>
     </div>
