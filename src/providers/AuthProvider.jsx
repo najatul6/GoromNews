c1519fc5d21902e90989e8b4f6ccb5d8efa20cc8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-29 22:17:36 +0600

    Create user with context and firebase

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react"
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import PropTypes from "prop-types";
 import app from "../firebase/firebase.config";
 
@@ -8,12 +8,16 @@ export const AuthContext=createContext(null)
 const auth = getAuth(app)
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null)
+
+    const createUser =(email,password) => {
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
     useEffect(()=>{
 
     },[])
     const authInfo={
         user,
-        setUser
+        createUser,
     }
   return (
     <AuthContext.Provider value={authInfo}>
