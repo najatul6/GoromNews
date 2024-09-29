8e0abec99bcb3a5f733a918a80dddd6e42b84076
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-29 22:25:42 +0600

    get current user from auth provider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,33 +1,39 @@
-import { createContext, useEffect, useState } from "react"
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
 import PropTypes from "prop-types";
 import app from "../firebase/firebase.config";
 
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-export const AuthContext=createContext(null)
-const auth = getAuth(app)
-const AuthProvider = ({children}) => {
-    const [user,setUser]=useState(null)
-
-    const createUser =(email,password) => {
-        return createUserWithEmailAndPassword(auth,email,password)
-    }
-    useEffect(()=>{
-
-    },[])
-    const authInfo={
-        user,
-        createUser,
-    }
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  const authInfo = {
+    user,
+    createUser,
+  };
   return (
-    <AuthContext.Provider value={authInfo}>
-        {children}
-    </AuthContext.Provider>
-  )
-}
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
 
 AuthProvider.propTypes = {
-  children: PropTypes.node.isRequired
-}
+  children: PropTypes.node.isRequired,
+};
 
-export default AuthProvider
+export default AuthProvider;
