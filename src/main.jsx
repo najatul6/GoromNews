ff4849d15d3e30ad330b319daf6126b1eea6b4d9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-30 20:16:16 +0600

    add toast container in main file for all routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Routers/Router";
 import AuthProvider from "./providers/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
+      <ToastContainer />
       <RouterProvider router={Router} />
     </AuthProvider>
   </StrictMode>
